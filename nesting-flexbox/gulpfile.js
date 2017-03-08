f2da9b90ace5bef4ffe6e76df3c53f2d8d70f384
Lisa Wells <lisawells999@gmail.com>
2017-03-08 12:47:49 -0500

    add livereload to gulpfile

--- nesting-flexbox/gulpfile.js
+++ nesting-flexbox/gulpfile.js
@@ -1,16 +1,22 @@
 var gulp = require('gulp');
 var autoprefixer = require('gulp-autoprefixer');
+var livereload = require('gulp-livereload');
 
 gulp.task('styles', function() {
     gulp.src('style.css')
         .pipe(autoprefixer())
-        .pipe(gulp.dest('build'));
+        .pipe(gulp.dest('build'))
+        .pipe(livereload());
 });
-
 // In terminal run “gulp styles”
+
 // watch task:
 // every time watch file is saved, named task is run
 
 gulp.task('watch', function() {
+
+    livereload.listen();
+
     gulp.watch('style.css', ['styles']);
 });
+// In terminal run “gulp watch”
